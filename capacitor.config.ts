55654d0d549373bcdac9763c26c6e046b9028bb9
DanMakor <danielmcauliffe@outlook.com>
2023-07-14 10:28:01 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'readonly-scroll-assist',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
